8d4f691992733e53d99eb664b2c9bd0275fd4e61
Mr.chu <chuhongkyu@gmail.com>
2024-05-15 20:23:45 +0900

    Fixed CSS

--- src/game.js
+++ src/game.js
@@ -80,7 +80,7 @@ const fontUrl = "./assets/fonts/Pretendard.json"
 
 
 // Renderer
-const canvas = document.querySelector("#three-canvas");
+const canvas = document.getElementById('three-canvas');
 const renderer = new THREE.WebGLRenderer({
   canvas,
   antialias: true,
