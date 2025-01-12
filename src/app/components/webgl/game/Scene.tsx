53340edc179586795f5b8ed7d5ccd8cc529cd551
Mr.chu <chuhongkyu@gmail.com>
2025-01-12 21:55:25 +0900

    Add Hole

--- src/app/components/webgl/game/Scene.tsx
+++ src/app/components/webgl/game/Scene.tsx
@@ -1,47 +1,71 @@
-"use client"
+"use client";
 
-import { Canvas } from "@react-three/fiber";
-import { Suspense, useEffect, useRef } from "react";
+import { Canvas, useThree } from "@react-three/fiber";
+import { Suspense, useEffect, useRef, useState } from "react";
 import Lights from "./Lights";
 import PlayerController from "./PlayerController";
+import { Hole } from "./map/Hole";
 
 function Scene() {
     const sectionRef = useRef<HTMLTableSectionElement>(null);
+    const [cameraSettings, setCameraSettings] = useState({
+        left: -1,
+        right: 1,
+        top: 1,
+        bottom: -1,
+        zoom: 0.25,
+    });
 
-    useEffect(()=>{
-        const timeout = setTimeout(()=> {
-          if(sectionRef?.current){
-            sectionRef.current.classList.add("hide")
-          }
-        }, 1000)
-    
-        return () => clearTimeout(timeout)
-    
-    },[sectionRef])
+    useEffect(() => {
+        const updateCameraSettings = () => {
+            const aspectRatio = window.innerWidth / window.innerHeight;
+            setCameraSettings({
+                left: -aspectRatio,
+                right: aspectRatio,
+                top: 1,
+                bottom: -1,
+                zoom: 0.25,
+            });
+        };
+
+        updateCameraSettings();
+        window.addEventListener("resize", updateCameraSettings);
+
+        return () => {
+            window.removeEventListener("resize", updateCameraSettings);
+        };
+    }, []);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            if (sectionRef?.current) {
+                sectionRef.current.classList.add("hide");
+            }
+        }, 1000);
+
+        return () => clearTimeout(timeout);
+    }, [sectionRef]);
 
     return (
         <section className="view" ref={sectionRef}>
-            <Canvas 
-                orthographic 
+            <Canvas
+                orthographic
                 shadows
                 camera={{
-                    position: [2,5,5],
-                    left: -(window.innerWidth / window.innerHeight),
-                    right: window.innerWidth / window.innerHeight,
-                    top: 1,
-                    bottom: -1,
+                    position: [2, 5, 5],
+                    ...cameraSettings,
                     near: -1000,
                     far: 1000,
-                    zoom: 0.25
                 }}
-                >
-                <Lights/>
+            >
+                <Lights />
                 <Suspense fallback={<></>}>
-                    <PlayerController/>
+                    <PlayerController />
+                    <Hole/>
                 </Suspense>
             </Canvas>
         </section>
-    )
+    );
 }
 
-export default Scene
+export default Scene;
---
+++ src/app/components/webgl/game/map/Hole.tsx
@@ -0,0 +1,30 @@
+import * as THREE from 'three'
+import React from 'react'
+import { useGLTF } from '@react-three/drei'
+import { GLTF } from 'three-stdlib'
+import { motion } from "framer-motion-3d"
+
+type GLTFResult = GLTF & {
+  nodes: {
+    Object_20: THREE.Mesh
+  }
+  materials: {
+    ['Material.007']: THREE.MeshStandardMaterial
+  }
+}
+
+export function Hole(props: JSX.IntrinsicElements['group']) {
+  const { nodes, materials } = useGLTF('/assets/models/hole.glb') as GLTFResult
+  return (
+    <group {...props} dispose={null}>
+      <motion.group 
+        initial={{y: 0}}
+        animate={{y: -2, transition: { duration: 0.5, delay: 2}}}
+        scale={0.68}>
+        <mesh receiveShadow castShadow geometry={nodes.Object_20.geometry} material={materials['Material.007']} position={[0, -0.069, 4]} />
+      </motion.group>
+    </group>
+  )
+}
+
+useGLTF.preload('/assets/models/hole.glb')
